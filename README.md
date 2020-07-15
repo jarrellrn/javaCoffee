dd5ae44b73aa3f55f7662f7045980cb7917cd165
jarrellrn <jarrellrn@gmail.com>
2020-07-15 15:44:38 -0500

    added stage 4 functionality

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # javaCoffee
 
-Just a short program following along with the JetBrains Academy course for Java. So far it takes in an integer for the amount of cups of coffee you'd like to make, and outputs the amount of ingredients you'll need. Very basic functionality at the moment.
+Just a short program following along with the JetBrains Academy course for Java. 
+
+Update July 15th, 2020:
+    Now takes in ingredient amounts as well as the amount of cups you'd like to make and tells you whether or not that is possible, with additional functionality for telling you how many cups it can make if you're under or over the maximum allowed.
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,19 +2,32 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Write how many cups of coffee you will need:");
+        System.out.println("Write how many ml of water the coffee machine has: ");
+        int water = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has: ");
+        int milk = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
+        int beans = scanner.nextInt();
+        System.out.println("Write how many cups of coffee you will need: ");
         int cups = scanner.nextInt();
 
-        int water = 200 * cups;
-        int milk = 50 * cups;
-        int beans = 15 * cups;
+        water /= 200; // gives amount of cups able to make based on this ingredient
+        milk /= 50; // gives amount of cups able to make based on this ingredient
+        beans /= 15; // gives amount of cups able to make based on this ingredient
 
-        System.out.println("For " + cups + " cups of coffee you will need:");
-        System.out.println(water + " ml of water");
-        System.out.println(milk + " ml of milk");
-        System.out.println(beans + " g of coffee beans");
+        int min = Math.min(water, milk);
+        min = Math.min(min, beans); // the maximum amount of cups we can make based on total ingredients
+
+        if (cups == min) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (cups < min) {
+            System.out.print("Yes, I can make that amount of coffee (and even " + (min - cups) + " more than that");
+        } else {
+            System.out.println("No, I can only make " + min + " cup(s) of coffee");
+        }
     }
 }
